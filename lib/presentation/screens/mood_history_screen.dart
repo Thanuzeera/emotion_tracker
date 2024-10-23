4da3bfc62debea4e567dc2f75e90cc393c089687
Thanuzeera Senthilkumar <thanuzeerasenthilkumar@gmail.com>
2024-10-23 16:31:49 +0530

    onboarding scren function added

--- lib/presentation/screens/mood_history_screen.dart
+++ lib/presentation/screens/mood_history_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class MoodHistoryScreen extends StatelessWidget {
+  MoodHistoryScreen({super.key});
   final moodBox = Hive.box('moodBox');
+
+  
 
   @override
   Widget build(BuildContext context) {
--- lib/presentation/screens/onboarding_screen.dart
+++ lib/presentation/screens/onboarding_screen.dart
@@ -1,7 +1,9 @@
 import 'package:emotiontrackerapp/constants/colors.dart';
 import 'package:emotiontrackerapp/data/mood_data.dart';
-import 'package:emotiontrackerapp/presentation/screens/mood_screen.dart';
+import 'package:emotiontrackerapp/presentation/screens/mood_history_screen.dart';
+
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:lottie/lottie.dart';
 import '../widgets/custom_button.dart';
@@ -15,11 +17,21 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   late LiquidController _liquidController;
+  late Box moodBox;
+  int _currentPage = 0; // Track the current page
 
+  void _logMood(MoodData mood) {
+    final now = DateTime.now();
+    moodBox.put(now.toString(), mood.moodText); 
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Mood Logged: ${mood.moodText}')),
+    );
+  }
 
   @override
   void initState() {
     _liquidController = LiquidController();
+    moodBox = Hive.box('moodBox');
     super.initState();
   }
 
@@ -101,19 +113,25 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             child: Padding(
               padding: const EdgeInsets.only(bottom: 80.0),
               child: CustomButton(
-                buttonText: moodData[0].buttonText,
+                buttonText: moodData[_currentPage].buttonText, // Use current page's mood
                 onPressed: () {
+                  // Log the mood of the current page
+                  _logMood(moodData[_currentPage]);
+
                   Navigator.push(
                     context,
                     PageRouteBuilder(
                       transitionDuration: Duration(milliseconds: 1500),
-                      pageBuilder: (context, animation, secondaryAnimation) => MoodScreen(),
-                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          MoodHistoryScreen(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
                         const begin = Offset(1.0, 0.0);
                         const end = Offset.zero;
                         const curve = Curves.ease;
 
-                        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
                         var offsetAnimation = animation.drive(tween);
 
                         return SlideTransition(
@@ -149,121 +167,13 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     }
   }
 
-  pageChangeCallback(int lpage) {
+  void pageChangeCallback(int page) {
     setState(() {
-      var page = lpage;
+      _currentPage = page; // Update current page when the page changes
     });
   }
 }
 
 
-// import 'package:emotiontrackerapp/data/mood_data.dart';
-// import 'package:flutter/material.dart';
-// import 'package:liquid_swipe/liquid_swipe.dart';
-// import 'package:lottie/lottie.dart';
 
-// import 'mood_screen.dart';
-
-// class OnboardingScreen extends StatefulWidget {
-//   @override
-//   _OnboardingScreenState createState() => _OnboardingScreenState();
-// }
-
-// class _OnboardingScreenState extends State<OnboardingScreen> {
-//   late LiquidController _liquidController;
-
-//   @override
-//   void initState() {
-//     _liquidController = LiquidController();
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Stack(
-//         children: [
-//           LiquidSwipe.builder(
-//             itemCount: moodData.length,
-//             itemBuilder: (context, index) {
-//               final data = moodData[index % moodData.length];
-//               return Container(
-//                 color: _getPageColor(index),
-//                 child: Column(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: [
-//                     Lottie.asset(
-//                       data.asset,
-//                       width: 250,
-//                       height: 250,
-//                       fit: BoxFit.cover,
-//                     ),
-//                     SizedBox(height: 40),
-//                     Text(
-//                       data.moodText,
-//                       style: TextStyle(
-//                         color: data.moodColor,
-//                         fontSize: 30,
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               );
-//             },
-//             liquidController: _liquidController,
-//             onPageChangeCallback: pageChangeCallback,
-//             waveType: WaveType.liquidReveal,
-//           ),
-//           Align(
-//             alignment: Alignment.bottomCenter,
-//             child: Padding(
-//               padding: const EdgeInsets.all(40.0),
-//               child: ElevatedButton(
-//                 onPressed: () {
-//                   Navigator.pushReplacement(
-//                     context,
-//                     MaterialPageRoute(builder: (context) => MoodScreen()),
-//                   );
-//                 },
-//                 style: ElevatedButton.styleFrom(
-//                   padding: EdgeInsets.symmetric(vertical: 15, horizontal: 100),
-//                   backgroundColor: Colors.black,
-//                   shape: RoundedRectangleBorder(
-//                     borderRadius: BorderRadius.circular(30),
-//                   ),
-//                 ),
-//                 child: Text(
-//                   "Get Started",
-//                   style: TextStyle(color: Colors.white, fontSize: 20),
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   void pageChangeCallback(int page) {
-//     setState(() {});
-//   }
-
-//   Color _getPageColor(int index) {
-//     switch (index) {
-//       case 0:
-//         return Colors.pinkAccent;
-//       case 1:
-//         return Colors.yellow;
-//       case 2:
-//         return Colors.lightBlue;
-//       case 3:
-//         return Colors.indigoAccent;
-//       case 4:
-//         return Colors.orange;
-//       default:
-//         return Colors.pinkAccent;
-//     }
-//   }
-// }
 
